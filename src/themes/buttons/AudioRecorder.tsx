4a7b74a489942df0f49a95e051a2aa8aed989d02
Akshay Bagai <akshaybagai52@gmail.com>
2023-08-07 12:44:32 +0530

    recorder

--- src/themes/buttons/AudioRecorder.tsx
+++ src/themes/buttons/AudioRecorder.tsx
@@ -1,80 +1,120 @@
-import React, { useState, useEffect } from 'react';
-import { View, Button } from 'react-native';
+import React, {useState} from 'react';
+import {View} from 'react-native';
 import AudioRecorderPlayer from 'react-native-audio-recorder-player';
+import {Button, ProgressBar, Text} from 'react-native-paper';
+const audioRecorderPlayer = new AudioRecorderPlayer();
 
 const Recorder = () => {
-  const audioRecorderPlayer = new AudioRecorderPlayer();
+  const [recordSecs, setRecordSecs] = useState(0);
+  const [recordTime, setRecordTime] = useState('');
+  const [currentPositionSec, setCurrentPositionSec] = useState(0);
+  const [currentDurationSec, setCurrentDurationSec] = useState(0);
+  const [playTime, setPlayTime] = useState('');
+  const [duration, setDuration] = useState('');
+
+  // after
   const [isRecording, setIsRecording] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
-  const [audioPath, setAudioPath] = useState('')
 
-  const startRecording = async () => {
-    try {
-      const result = await audioRecorderPlayer.startRecorder();
-      console.log('Recording started:', result);
-      setIsRecording(true);
-    } catch (err) {
-      console.log('Error starting recording:', err);
-    }
+  const onStartRecord = async () => {
+    const result = await audioRecorderPlayer.startRecorder();
+    setIsRecording(true);
+    audioRecorderPlayer.addRecordBackListener(e => {
+      setRecordSecs(e.currentPosition);
+      setRecordTime(audioRecorderPlayer.mmssss(Math.floor(e.currentPosition)));
+    });
+    console.log(result);
   };
 
-  const stopRecording = async () => {
-    try {
-      const result = await audioRecorderPlayer.stopRecorder();
-      console.log('Recording stopped:', result);
-      setIsRecording(false);
-    } catch (err) {
-      console.log('Error stopping recording:', err);
-    }
+  const onStopRecord = async () => {
+    const result = await audioRecorderPlayer.stopRecorder();
+    audioRecorderPlayer.removeRecordBackListener();
+    setIsRecording(false);
+
+    setRecordSecs(0);
+    console.log(result);
   };
 
-  const startPlayback = async () => {
-    try {
-      audioRecorderPlayer.setVolume(1.0); // Set the volume before starting the playback
-      console.log('Playback started');
-      audioRecorderPlayer.addPlayBackListener((e: any) => {
-        if (e.current_position === e.duration) {
-          console.log('Playback finished');
-          setIsPlaying(false);
-          audioRecorderPlayer.stopPlayer();
-        }
-      });
-      const path = await audioRecorderPlayer.startPlayer(); // Pass the audio path here
-      console.log('Audio Path:', path);
+  const onStartPlay = async () => {
+    console.log('onStartPlay');
+    const msg = await audioRecorderPlayer.startPlayer();
+    console.log(msg);
+    audioRecorderPlayer.addPlayBackListener(e => {
+      setCurrentPositionSec(e.currentPosition);
+      setCurrentDurationSec(e.duration);
+      setPlayTime(audioRecorderPlayer.mmssss(Math.floor(e.currentPosition)));
+      setDuration(audioRecorderPlayer.mmssss(Math.floor(e.duration)));
       setIsPlaying(true);
-    } catch (err) {
-      console.log('Error starting playback:', err);
-    }
+    });
   };
 
-  const stopPlayback = async () => {
-    try {
-      console.log('Playback stopped');
-      setIsPlaying(false);
-      audioRecorderPlayer.stopPlayer();
-    } catch (err) {
-      console.log('Error stopping playback:', err);
-    }
+  const onPausePlay = async () => {
+    await audioRecorderPlayer.pausePlayer();
+     setIsPlaying(false);
   };
 
-  useEffect(() => {
-    return () => {
-      // Clean up the playback listener when the component unmounts
-      audioRecorderPlayer.removePlayBackListener();
-    };
-  }, []);
+  const onStopPlay = () => {
+    console.log('onStopPlay');
+    audioRecorderPlayer.stopPlayer();
+    audioRecorderPlayer.removePlayBackListener();
+    setIsPlaying(false);
+  };
 
   return (
     <View>
-      <Button
-        title={isRecording ? 'Stop Recording' : 'Start Recording'}
-        onPress={isRecording ? stopRecording : startRecording}
-      />
-      <Button
-        title={isPlaying ? 'Stop Playback' : 'Start Playback'}
-        onPress={isPlaying ? stopPlayback : startPlayback}
-        disabled={isRecording}
-      />
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          width: '90%',
+        }}>
+        <Button
+          icon={isRecording ? 'stop' : 'record'}
+          onPress={isRecording ? onStopRecord : onStartRecord}
+          disabled={isPlaying}
+          mode="contained">
+          {isRecording ? 'Stop Recording' : 'Start Recording'}
+          {/* <IconRecord name={isRecording ? 'controller-stop' : 'controller-record'} /> */}
+        </Button>
+        <Text>{recordTime}</Text>
+      </View>
+      {/* <Text>Record Secs: {recordSecs}</Text> */}
+
+      {/* <Text>Current Position Sec: {currentPositionSec}</Text> */}
+      {/* <Text>Current Duration Sec: {currentDurationSec}</Text> */}
+      {/* <Text>Play Time: {playTime}</Text> */}
+      {/* <ProgressBar
+        progress={currentPositionSec / currentDurationSec}
+        color="#112D4E"
+        style={{height: 8, borderRadius: 5, marginTop: 10, width: '90%'}}
+      /> */}
+      <View
+        style={{
+          marginTop: 10,
+          flexDirection: 'row',
+          width: '90%',
+          justifyContent: 'space-between',
+        }}>
+        <Button
+          icon={isPlaying ? 'play-pause' : 'play-circle'}
+          onPress={isPlaying ? onPausePlay : onStartPlay}
+          disabled={isRecording}
+          // style={{width: '90%'}}
+          mode="contained">
+          {isPlaying ? 'Pause' : 'Play'}{' '}
+        </Button>
+        <Button
+          icon={'restart'}
+          onPress={onStopPlay}
+          disabled={isRecording}
+          // style={{width: '90%'}}
+          mode="contained">
+          Restart
+        </Button>
+      </View>
+
+      {/* <Text>Duration: {duration}</Text> */}
     </View>
   );
 };
