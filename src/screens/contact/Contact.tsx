b6cbb5c675c4d31bf7848caeb31091de05ba92cc
Akshay Bagai <akshaybagai52@gmail.com>
2023-08-29 16:58:23 +0530

    added formik

--- src/screens/contact/Contact.tsx
+++ src/screens/contact/Contact.tsx
@@ -9,7 +9,7 @@ import {Button, Text} from 'react-native-paper';
 import React from 'react';
 import commonStyles from '../../styles/commonStyles';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import {Formik} from 'formik';
+import {Formik,ErrorMessage} from 'formik';
 import * as yup from 'yup';
 import {TextInput} from 'react-native';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
@@ -67,7 +67,7 @@ const Contact = () => {
   };
   const validationSchema = yup.object({
     name: yup.string().required('Name is required'),
-    email: yup.string().required('Name is required'),
+    email: yup.string().required('Email is required'),
     subject: yup.string().required('Subject can"t be empty '),
     message: yup.string().required('message is required'),
   });
@@ -143,49 +143,63 @@ const Contact = () => {
           <Formik
             initialValues={initialValues}
             validationSchema={validationSchema}
-            onSubmit={() => console.log('first')}>
-            {({isSubmitting, handleChange}) => (
+            onSubmit={(values) => console.log('first', values)}>
+           {({isSubmitting, handleChange, handleSubmit,touched, errors}) => {
+              const handlePress = () => {
+                handleSubmit(); // Call Formik's handleSubmit function here
+              };
+              return (
+
               <KeyboardAvoidingView
                 behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
                 style={{flex: 1}}>
                 <View style={[{gap: 20}]}>
                   <TextInput
-                    onChangeText={handleChange('name')}
+                    onChangeText={handleChange('email')}
                     placeholder="Email"
-                    style={commonStyles.textInput}
-                    // placeholderTextColor="#47b2e4"
+                    style={[commonStyles.textInput, {borderColor: touched.email && errors.email ? 'red' : "#ccc"}]}
                   />
+                  {touched.email && errors.email && (<Text style={commonStyles.errorText}>{errors.email}</Text>)}
+                  {/* <ErrorMessage component={Text} /> */}
                   <TextInput
                     onChangeText={handleChange('name')}
                     placeholder="Name"
-                    style={commonStyles.textInput}
-                    // placeholderTextColor="#47b2e4"
+                    style={[commonStyles.textInput, {borderColor: touched.name && errors.name ? 'red' : "#ccc"}]}
+
                   />
+                  {touched.name && errors.name && (<Text style={commonStyles.errorText}>{errors.name}</Text>)}
+
                   <TextInput
-                    onChangeText={handleChange('name')}
+                    onChangeText={handleChange('subject')}
                     placeholder="Subject"
-                    style={commonStyles.textInput}
-                    // placeholderTextColor="#47b2e4"
+                    style={[commonStyles.textInput, {borderColor: touched.subject && errors.subject ? 'red' : "#ccc"}]}
+
                   />
+                  {touched.subject && errors.subject && (<Text style={commonStyles.errorText}>{errors.subject}</Text>)}
+
                   <TextInput
-                    onChangeText={handleChange('name')}
+                    onChangeText={handleChange('message')}
                     placeholder="Message"
-                    style={commonStyles.textArea}
+                    style={[commonStyles.textArea, {borderColor: touched.message && errors.message ? 'red' : "#ccc"}]}
+
                     multiline={true}
                     numberOfLines={4}
-                    // placeholderTextColor="#47b2e4"
                   />
+                  {touched.message && errors.message && (<Text style={commonStyles.errorText}>{errors.message}</Text>)}
+
                 </View>
                 <Button
                   mode="elevated"
                   style={[styles.buttonStyles, commonStyles.mTop15]}
                   textColor="#47b2e4"
                   rippleColor="#47b2e4"
-                  onPress={() => console.log('first')}>
+                  onPress={handlePress}
+          >
                   Send Message
                 </Button>
               </KeyboardAvoidingView>
-            )}
+               )
+              }}
           </Formik>
         </View>
       </ScrollView>
